6c7cea64e148bdc51517c9a130646e7d304c9f80
vladislav-andriushchenko <vladitheboy@gmail.com>
2025-04-17 21:42:08 +0400

    added logger

--- src/test/java/tests/AnnotatedTests.java
+++ src/test/java/tests/AnnotatedTests.java
@@ -1,9 +1,12 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import steps.GitRepoSteps;
@@ -12,6 +15,13 @@ import utils.TestBase;
 public class AnnotatedTests extends TestBase {
 
     GitRepoSteps steps = new GitRepoSteps();
+
+    @BeforeAll
+    static void setupAllure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true));
+    }
 
     @Epic("GitHub UI Tests")
     @Feature("Issues Tab")
